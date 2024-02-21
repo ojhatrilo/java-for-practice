e8eb8bf00cd15ebff0931659af3800e89331596b
ojhatrilo <115971421+ojhatrilo@users.noreply.github.com>
2024-02-21 19:13:46 +0530

    today class

---
+++ FramWork-1/part-1/src/main/java/org/example/StandAloneTest.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import dev.failsafe.internal.util.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class StandAloneTest {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://rahulshettyacademy.com/client");
+
+        driver.findElement(By.id("userEmail")).sendKeys("livewire@gmail.com");
+        driver.findElement(By.id("userPassword")).sendKeys("Admin@123");
+        driver.findElement(By.id("login")).click();
+
+        List<WebElement> products = driver.findElements(By.cssSelector(".mb-3"));
+
+        WebElement prod = products.stream().filter(product ->
+                product.findElement(By.cssSelector("b")).getText().equals("ZARA COAT 3")).findFirst().orElse(null);
+        prod.findElement(By.cssSelector(".card-body button:last-of-type")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#toast-container")));
+
+        wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
+        driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
+
+
+        List<WebElement> cartProducts = driver.findElements(By.cssSelector(".cartsection h3"));
+
+        Boolean match = cartProducts.stream().anyMatch(cartProduct-> cartProduct.getText().equals("ZARA COAT 3"));
+
+        Assert.assertTrue(match);
+
+        driver.findElement(By.cssSelector(".totalRow button")).click();
+
+
+
+
+
+
+
+    }
+}
