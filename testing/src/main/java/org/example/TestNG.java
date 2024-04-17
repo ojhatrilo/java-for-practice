ff75dce512862ccc678d02e34f10972980dd3e71
Ojhatrilo <ojhatrilochan5@gmail.com>
2024-04-17 16:03:03 +0530

    comit

--- testing/src/main/java/org/example/TestNG.java
+++ testing/src/main/java/org/example/TestNG.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class TestNG {
@@ -8,4 +10,13 @@ public class TestNG {
         System.out.println("hi");
 
     }
+    @BeforeTest
+    public void june(){
+        System.out.println("hi everyone");
+    }
+    @AfterTest
+    public void guest(){
+        System.out.println("hello guest");
+    }
+
 }
